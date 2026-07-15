d0478e95d5748c017429bb7abb936f749cdc5a6e
shivamgohar <shivamgohar.sg@gmail.com>
2026-07-15 20:06:01 +0530

    Completed CRM core modules with dashboard and order management

--- client/src/components/DashboardCard.jsx
+++ client/src/components/DashboardCard.jsx
@@ -1,17 +1,36 @@
-function DashboardCard({ title, value }) {
+import {
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
+
+function DashboardCard({ title, value, icon }) {
   return (
-    <div
-      style={{
-        width: "220px",
-        padding: "20px",
-        background: "#ffffff",
-        borderRadius: "10px",
-        boxShadow: "0px 2px 8px rgba(0,0,0,0.2)",
+    <Card
+      elevation={3}
+      sx={{
+        borderRadius: 3,
+        height: 180,
       }}
     >
-      <h3>{title}</h3>
-      <h1>{value}</h1>
-    </div>
+      <CardContent>
+
+        <Typography variant="h6">
+          {icon} {title}
+        </Typography>
+
+        <Typography
+          variant="h3"
+          color="primary"
+          align="center"
+          mt={4}
+          fontWeight="bold"
+        >
+          {value}
+        </Typography>
+
+      </CardContent>
+    </Card>
   );
 }
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,112 +1,74 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import DashboardCard from "../components/DashboardCard";
+
+import { Box, Typography, Grid, Card, CardContent } from "@mui/material";
 
 function Dashboard() {
-
   const [stats, setStats] = useState({
-    customers: 0,
-    products: 0,
-    orders: 0,
-    revenue: 0,
+  customers: 0,
+  products: 0,
+  orders: 0,
+  revenue: 0,
   });
 
   const fetchDashboard = async () => {
-
     try {
-
-      const response = await axios.get(
-        "http://localhost:5000/dashboard"
-      );
+      const response = await axios.get("http://localhost:5000/dashboard");
+       
 
       setStats(response.data);
-
     } catch (error) {
-
       console.error(error);
-
     }
-
-  };
+  };  
 
   useEffect(() => {
-
     fetchDashboard();
-
   }, []);
 
   return (
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h3" fontWeight="bold" mb={4}>
+        CRM Dashboard
+      </Typography>
+<Grid container spacing={3}>
 
-    <div style={{ padding: "30px" }}>
+  <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+    <DashboardCard
+      title="Customers"
+      value={stats.customers}
+      icon="👥"
+    />
+  </Grid>
 
-      <h1>CRM Dashboard</h1>
+  <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+    <DashboardCard
+      title="Products"
+      value={stats.products}
+      icon="📦"
+    />
+  </Grid>
 
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(2,250px)",
-          gap: "20px",
-          marginTop: "30px",
-        }}
-      >
+  <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+    <DashboardCard
+      title="Orders"
+      value={stats.orders}
+      icon="🛒"
+    />
+  </Grid>
 
-        <div
-          style={{
-            border: "1px solid #ddd",
-            padding: "20px",
-            borderRadius: "10px",
-          }}
-        >
-          <h2>👥 Customers</h2>
+  <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+    <DashboardCard
+      title="Revenue"
+      value={`₹ ${stats.revenue}`}
+      icon="💰"
+    />
+  </Grid>
 
-          <h1>{stats.customers}</h1>
-
-        </div>
-
-        <div
-          style={{
-            border: "1px solid #ddd",
-            padding: "20px",
-            borderRadius: "10px",
-          }}
-        >
-          <h2>📦 Products</h2>
-
-          <h1>{stats.products}</h1>
-
-        </div>
-
-        <div
-          style={{
-            border: "1px solid #ddd",
-            padding: "20px",
-            borderRadius: "10px",
-          }}
-        >
-          <h2>🛒 Orders</h2>
-
-          <h1>{stats.orders}</h1>
-
-        </div>
-
-        <div
-          style={{
-            border: "1px solid #ddd",
-            padding: "20px",
-            borderRadius: "10px",
-          }}
-        >
-          <h2>💰 Revenue</h2>
-
-          <h1>₹ {stats.revenue}</h1>
-
-        </div>
-
-      </div>
-
-    </div>
-
+</Grid>
+    </Box>
   );
-
 }
 
 export default Dashboard;
